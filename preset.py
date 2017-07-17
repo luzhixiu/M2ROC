7b2e3c143b4a768527b06c2e531574b5294fd560
lu <luzhixiu1996@gmail.com>
2017-07-17 13:25:15 -0500

    new file:   Cross Valiadation Config.txt 	new file:   CrossValidation - Copy.py 	new file:   CrossValidation.py 	new file:   Input 	new file:   Input(CrossValidation)/Information Gain Ratio.csv 	new file:   Input(CrossValidation)/Information Gain.csv 	new file:   Input(CrossValidation)/Relief.csv 	new file:   Input(CrossValidation)/mrmr(MID).csv 	new file:   Input(CrossValidation)/mrmr(MIQ).csv 	new file:   Input(LeaveOneOut)/Information Gain.csv 	new file:   InputLeaveOneOutfile 	new file:   LeaveOneOut.py 	deleted:    PipeLine/.LeaveOneOut.py.swp 	modified:   PipeLine/Cross Valiadation Config.txt 	modified:   PipeLine/LeaveOneOut.py 	modified:   PipeLine/LeaveOneOutConfig.txt 	modified:   PipeLine/Output(LeaveOneOut)/SVM (Unbalanced 15).png 	new file:   PipeLine/Output(LeaveOneOut)/SVM TOP 1 Features.png 	modified:   PipeLine/Output(LeaveOneOut)/SVM TOP 10 Features.png 	new file:   PipeLine/Output(LeaveOneOut)/SVM TOP 11 Features.png 	modified:   PipeLine/Output(LeaveOneOut)/SVM TOP 20 Features.png 	new file:   PipeLine/Output(LeaveOneOut)/SVM TOP 22 Features.png 	modified:   PipeLine/Output(LeaveOneOut)/SVM TOP 30 Features.png 	new file:   download.jpg 	modified:   form.html 	new file:   home_lu_webProject_Input 	new file:   home_lu_webProject_InputLeaveOneOut 	new file:   preset.py

---
+++ preset.py
@@ -0,0 +1,61 @@
+from flask import Flask, redirect, url_for, request,send_file
+import configparser
+from werkzeug import secure_filename
+import os
+from subprocess import call
+
+app = Flask(__name__)
+
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome! %s' % name
+    
+@app.route('/get_image')
+def get_image():
+    if request.args.get('type') == '1':
+       filename = 'download.jpg'
+    else:
+       filename = 'download.jpg'
+    return send_file(filename, mimetype='image/jpg')    
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        f=request.files['file']
+        workdir=os.path.join(os.getcwd(), 'Input(LeaveOneOut)',f.filename)
+        print workdir     
+        f.save(workdir)
+        classifier = request.form['classifier']
+        estimators=request.form['estimators']
+        avg=request.form['avg']
+        interval=request.form['interval']
+        plotrange=request.form['plotrange']
+        legendsize=request.form['legendsize']
+        plotlinewidth=request.form['plotlinewidth']
+        legendtitle=request.form['legendtitle']
+        testoutput=classifier+" "+estimators+" "+avg+" "+interval+" "+plotrange+" "+legendsize+" "+plotlinewidth+" "+legendtitle        
+        settings = configparser.ConfigParser()
+        settings._interpolation = configparser.ExtendedInterpolation()
+        settings.read('LeaveOneOutConfig.txt')
+        settings.set('SectionOne', 'Classifier', str(classifier))   
+        settings.set('SectionOne', 'number of estimators', str(estimators))
+        settings.set('SectionOne', 'average the result', str(avg))
+        settings.set('SectionOne', 'feature selection interval', str(interval)) 
+        settings.set('SectionOne', 'plot feature range', str(plotrange))
+        settings.set('SectionOne', 'plot lengend size', str(legendsize)) 
+        settings.set('SectionOne', 'plot line width', str(plotlinewidth)) 
+        settings.set('SectionOne', 'dataset type name', str(legendtitle))         
+        call(["python", "LeaveOneOut.py"])
+        
+        
+        
+        
+        
+        with open('LeaveOneOutConfig.txt', 'wb') as configfile:
+            settings.write(configfile)        
+        
+        return redirect(url_for('get_image'))
+        return redirect(url_for('success',name =testoutput ))
+if __name__ == '__main__':
+    app.run(debug = True)
+
