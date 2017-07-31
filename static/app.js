e08b177d3f7e9301a8a9b6ce65fc0195dbbd89ef
Ubuntu <ubuntu@ip-172-26-12-182.us-east-2.compute.internal>
2017-07-31 06:24:45 +0000

    page one done

--- static/app.js
+++ static/app.js
@@ -3,7 +3,11 @@ function msg(){
 }  
 
 function  loadSample(){
-   document.getElementById("p1").innerHTML = "Using Sample File";
+   document.getElementById("p2").innerHTML = "Sample File Loaded as Input";
+   document.getElementById("load").style.visibility = "hidden"; 	
+   document.getElementById("file").style.visibility = "hidden";
+   document.getElementById("or").style.visibility = "hidden";   
+   document.getElementById("p1").style.visibility = "hidden";
 
 }
 
